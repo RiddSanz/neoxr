4dde290e96dbd137ce50ea64c0762f26d664d6b1
Saint Jameson Parid <82357480+RiddSanz@users.noreply.github.com>
2025-04-02 22:15:32 +0700

    editing smth

--- plugins/misc/owner.js
+++ plugins/misc/owner.js
@@ -11,9 +11,9 @@ exports.run = {
          number: env.owner,
          about: 'Owner & Creator'
       }], m, {
-         org: 'Neoxr Network',
-         website: 'https://api.neoxr.my.id',
-         email: 'contact@neoxr.my.id'
+         org: 'Parid Network Corp.',
+         website: 'https://paridx.my.id',
+         email: 'saintparid@gmail.com'
       })
    },
    error: false,
